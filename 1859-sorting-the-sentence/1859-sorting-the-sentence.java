7c9b5bf85fe013cdd3ec52b7c1dcdd4060b18bae
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-15 23:49:40 +0300

    Time: 9 ms (13.22%), Space: 42.7 MB (6.95%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -2,15 +2,13 @@ class Solution {
     public String sortSentence(String s) {
       String result = "";
       int i=1;
-      s=" "+s;
       while(s.indexOf(String.valueOf(i))!=-1){
           int j=s.indexOf(String.valueOf(i));
           int k=j-2;
-          while(s.charAt(k)!=' '){
+          while(k>=0 && s.charAt(k)!=' '){
               k--;
           }
-          result += s.substring(k,j);
-          // s=s.replace()
+          result += s.substring(k+1,j)+" ";
           i++;
       }
      return result.trim();
