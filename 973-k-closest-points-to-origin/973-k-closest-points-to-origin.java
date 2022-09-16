46b0168e0253b0fe4dcd0d71224e3bfea37bf960
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-16 16:50:49 +0300

    Time: 73 ms (28.81%), Space: 114.4 MB (76.60%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,7 +1,15 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points, Comparator.comparingInt(v -> v[0] * v[0] + v[1] * v[1]));        
+        Arrays.sort(points, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {  
+                int x= (int)(Math.pow(a[0],2) + Math.pow(a[1],2));
+                int y= (int)(Math.pow(b[0],2) + Math.pow(b[1],2));
+                return x-y;
+            }
+        });        
         
         return Arrays.copyOf(points, k);
     }
 }
+
+        // Arrays.sort(points, Comparator.comparingInt(v -> v[0] * v[0] + v[1] * v[1]));        
