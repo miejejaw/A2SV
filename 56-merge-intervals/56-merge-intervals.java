d2b19ee6f44f14fa2a7ea8114be42ca8e1891b78
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-16 17:53:16 +0300

    Time: 19 ms (22.54%), Space: 55.2 MB (51.64%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[][] merge(int[][] interval) {
         Arrays.sort(interval,Comparator.comparingInt(v->v[0]));
-        int[][] result = new int[interval.length][2];
         int k=0,x=interval[0][0],y=interval[0][1];
-        
-        
+           
         for(int i=1; i<interval.length;){
             if(interval[i][0]>y){
-                result[k][0] = x;
-                result[k][1] = y; 
+                interval[k][0] = x;
+                interval[k][1] = y; 
                 k++;
                 x=interval[i][0];
                 y=interval[i][1];
@@ -20,9 +18,9 @@ class Solution {
             }
             
         }
-        result[k][0] = x;
-        result[k][1] = y; 
+        interval[k][0] = x;
+        interval[k][1] = y; 
         
-        return Arrays.copyOf(result,k+1);
+        return Arrays.copyOf(interval,k+1);
     }
 }
