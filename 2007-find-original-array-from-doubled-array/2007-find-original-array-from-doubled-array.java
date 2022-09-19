9a4f6f15f8bd7ecb7b5d721c66bd40d92a0abd9f
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-19 09:06:10 +0300

    Time: 119 ms (66.98%), Space: 133.4 MB (48.90%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.java
@@ -1,25 +1,18 @@
 class Solution {
     public int[] findOriginalArray(int[] changed) {
         if(changed.length%2!=0) return new int[0];
-        int[] result = new int[changed.length/2];
-        Arrays.sort(changed);
-        Queue<Integer> temp = new LinkedList<>();
         
-        for(int i=0,k=0; i<changed.length; i++){
-
-            if(!temp.isEmpty() && temp.peek()==changed[i]){
-                temp.poll();
-            }
-            else if(!temp.isEmpty() && temp.peek()<changed[i] || k>=result.length){
-                return new int[0];
-            }
-            else {
-                result[k]=changed[i];
-                temp.add(changed[i]*2);
-                k++;
+        Queue<Integer> temp = new LinkedList<>();
+        Arrays.sort(changed);
+        
+        for(int i=changed.length-1,k=changed.length-1; i>=0; i--){
+            if(!temp.isEmpty() && temp.peek()==changed[i]*2) temp.poll();
+            else{
+                temp.add(changed[i]);
+                changed[k]=changed[i]/2;
+                k--;
             }
         }
- 
-        return result;
+      return (temp.isEmpty())? Arrays.copyOfRange(changed,changed.length/2,changed.length): new int[0];
     }
 }
