771462e3ce75c17c35414f78e5dc504cd0a813f4
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-20 14:58:36 +0300

    Time: 40 ms (54.87%), Space: 78 MB (6.91%) - LeetHub

--- 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
+++ 1561-maximum-number-of-coins-you-can-get/1561-maximum-number-of-coins-you-can-get.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxCoins(int[] piles) {
         Arrays.sort(piles);
         int result=0;
-        for(int i=piles.length-2; i>=piles.length/3; i-=2){
+        for(int i=piles.length/3; i<piles.length; i+=2){
             result+=piles[i];
         }
         return result;
