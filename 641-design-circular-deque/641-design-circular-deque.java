b144798e90caeb248882fe7b60395ad5c50fa13f
mie21 <103285322+mie21@users.noreply.github.com>
2022-09-23 13:50:55 +0300

    Time: 12 ms (13.43%), Space: 48.3 MB (70.36%) - LeetHub

---
+++ 641-design-circular-deque/641-design-circular-deque.java
@@ -0,0 +1,61 @@
+class MyCircularDeque {
+    int maxSize;
+    LinkedList<Integer> q;
+    public MyCircularDeque(int k) {
+        maxSize=k;
+        q=new LinkedList<>();
+    }
+    
+    public boolean insertFront(int value) {
+        if(q.size()<maxSize){
+            q.addFirst(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean insertLast(int value) {
+        if(q.size()<maxSize){
+            q.addLast(value);
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean deleteFront() {
+        return (q.pollFirst()!=null)?true:false;      
+    }
+    
+    public boolean deleteLast() {      
+        return (q.pollLast()!=null)?true:false;
+    }
+    
+    public int getFront() {
+        return (q.isEmpty())?-1:q.peekFirst();
+    }
+    
+    public int getRear() {
+        return (q.isEmpty())?-1:q.peekLast();
+    }
+    
+    public boolean isEmpty() {
+        return q.isEmpty();
+    }
+    
+    public boolean isFull() {
+        return (q.size()==this.maxSize)?true:false;
+    }
+}
+
+/**
+ * Your MyCircularDeque object will be instantiated and called as such:
+ * MyCircularDeque obj = new MyCircularDeque(k);
+ * boolean param_1 = obj.insertFront(value);
+ * boolean param_2 = obj.insertLast(value);
+ * boolean param_3 = obj.deleteFront();
+ * boolean param_4 = obj.deleteLast();
+ * int param_5 = obj.getFront();
+ * int param_6 = obj.getRear();
+ * boolean param_7 = obj.isEmpty();
+ * boolean param_8 = obj.isFull();
+ */
