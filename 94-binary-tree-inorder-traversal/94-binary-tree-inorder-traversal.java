472f26780bd1ccdd74f420fa8681c2cdc96913c2
mie21 <103285322+mie21@users.noreply.github.com>
2022-10-08 12:23:01 +0300

    Time: 0 ms (100.00%), Space: 42.5 MB (19.84%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -1,14 +1,11 @@
 class Solution {
     List<Integer> result = new LinkedList<>();
     public List<Integer> inorderTraversal(TreeNode root) {
-        helper(root);
+        if(root!=null){
+            inorderTraversal(root.left);
+            result.add(root.val);
+            inorderTraversal(root.right);
+        }
         return result;  
-    }
-    public void helper(TreeNode root){
-        if(root==null) return;
-        
-        helper(root.left);
-        result.add(root.val);
-        helper(root.right);
     }
 }
