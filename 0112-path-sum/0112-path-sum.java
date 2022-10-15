85a8a14cd278660865a30f87b9a9877053ba8ae9
mie21 <103285322+mie21@users.noreply.github.com>
2022-10-15 11:58:36 +0300

    Time: 1 ms (75.06%), Space: 44.1 MB (13.09%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    boolean ans =false;
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        helper(root,targetSum);
+        return ans;
+    }
+    public void helper(TreeNode root, int target){
+        if(root==null || ans) return;
+        target-=root.val;
+        if(target==0 && root.left==null && root.right==null)
+            ans=true;
+        helper(root.left,target);
+        helper(root.right,target);
+    }
+}
