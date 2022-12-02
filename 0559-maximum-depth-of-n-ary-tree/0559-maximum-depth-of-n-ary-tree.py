e496ca2a71d4ef352e949618a5327a125a7b7a04
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-02 10:55:54 +0300

    Time: 46 ms (94.37%), Space: 16.1 MB (53.12%) - LeetHub

--- 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
+++ 0559-maximum-depth-of-n-ary-tree/0559-maximum-depth-of-n-ary-tree.py
@@ -1,20 +1,8 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
-
 class Solution:
     def maxDepth(self, root: 'Node') -> int:
-        ans = 0
-        def helper(root: 'Node', level: int):
-            if not root: return
-            nonlocal ans
-            ans = max(ans,level)
-            for i in root.children:
-                helper(i,level+1)
-        helper(root,1)
-        return ans
+        if root == None: return 0
+        depth = 0
         
+        for child in root.children:
+            depth = max(depth, self.maxDepth(child))        
+        return depth + 1
