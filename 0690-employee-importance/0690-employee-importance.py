8462aae7f0595b17da9abaf79090b0337f1ea803
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-02 12:02:17 +0300

    Time: 154 ms (90.83%), Space: 15.6 MB (26.96%) - LeetHub

--- 0690-employee-importance/0690-employee-importance.py
+++ 0690-employee-importance/0690-employee-importance.py
@@ -1,22 +1,13 @@
-"""
-# Definition for Employee.
-class Employee:
-    def __init__(self, id: int, importance: int, subordinates: List[int]):
-        self.id = id
-        self.importance = importance
-        self.subordinates = subordinates
-"""
-
 class Solution:
     def getImportance(self, employees: List['Employee'], id: int) -> int:
-        total = 0
-        for i,val in enumerate(employees):
-            if val.id == id:
-                id = i
-                break            
-        for i in employees[id].subordinates:
-            total += self.getImportance(employees,i)
-        total += employees[id].importance
+        total = 0 
+        emps= {emp.id: emp for emp in employees}
+        def helper(id: int):
+            nonlocal total
+            total += emps[id].importance
+            for i in emps[id].subordinates:
+                helper(i)
+        helper(id)
         return total
         
 #10 57
