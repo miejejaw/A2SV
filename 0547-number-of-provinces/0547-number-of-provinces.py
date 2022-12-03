179ea237a650e457a6935bee43ff7a3617d6f348
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-03 16:09:23 +0300

    Time: 311 ms (72.48%), Space: 19.8 MB (5.11%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,17 +1,17 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        def helper(i: int):            
+        ans = 0
+        def helper(i): 
             while 1 in isConnected[i]:           
                 j = isConnected[i].index(1)
-                isConnected[i][j] = 2
-                isConnected[j][i] = 2
+                isConnected[i][j] = 0
+                isConnected[j][i] = 0
                 helper(j)
-            return 1
-        ans = 0
         for i in range(len(isConnected)):           
             if isConnected[i][i]==1:
-                isConnected[i][i]=2
-                ans += helper(i)
+                isConnected[i][i] = 0
+                helper(i)
+                ans += 1
         return ans
         
         
