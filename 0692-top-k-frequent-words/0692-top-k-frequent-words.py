7970920ba1774e812d39c21bf5c65e2bdbcac181
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-13 10:34:39 +0300

    Time: 119 ms (52.49%), Space: 14.1 MB (24.94%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -2,19 +2,11 @@ from queue import PriorityQueue
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         q,ans = PriorityQueue(),[]
-        temp = [(freq,st) for st,freq in Counter(words).items()]
-        temp.sort( key=lambda i: (-i[0],i[1]))
-        for freq,st in temp:
-            temp = (freq,st)           
-            if q.qsize()>k-1: 
-                pop = q.get()
-                if pop[0]>temp[0] or (pop[0]==temp[0] and temp[1]>pop[1]):
-                    temp = pop              
-            q.put(temp)
-
-        while not q.empty():
-            ans.append(q.get())
-        ans.sort( key=lambda i: i[0],reverse=True)   
-        return [st for fre,st in ans]
+        for st,freq in Counter(words).items():
+            q.put((-freq,st))
+        for _ in range(k):
+            ans.append(q.get()[1])
+  
+        return ans
 
         
