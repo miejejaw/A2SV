cf5f8fec6f70114aa3cb2a097dd4c32489430eff
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-22 16:40:44 +0300

    Time: 42 ms (72.30%), Space: 13.9 MB (68.11%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.py
@@ -3,9 +3,9 @@ class Solution:
         i,ans = len(s)-1,''
         while i >= 0:
             if '#' == s[i]:
-                ans = chr(96+int(s[i-2]+s[i-1])) + ans
-                i -= 2
+                ans = chr(96+int(s[i-2:i])) + ans
+                i -= 3
             else: 
                 ans = chr(96+int(s[i])) + ans
-            i -= 1
+                i -= 1
         return ans
