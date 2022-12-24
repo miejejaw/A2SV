d1a6510a5222528659a13ea0cabb5a1b9230843e
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-24 09:36:48 +0300

    Time: 22 ms (99.75%), Space: 13.8 MB (96.47%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -3,5 +3,4 @@ class Solution:
         min_salary = min(salary)
         max_salary = max(salary)
         total_salary = sum(salary)-min_salary-max_salary
-        average_salary = round(total_salary / (len(salary)-2) , 5)
-        return average_salary
+        return total_salary / (len(salary)-2)
