235c89935daacb1349d55eae26d1cd7f67663206
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-24 09:28:38 +0300

    Time: 37 ms (80.91%), Space: 13.8 MB (54.68%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.py
@@ -3,5 +3,5 @@ class Solution:
         salary.sort()
         total_salary = sum(salary)
         total_salary -= salary[0]+salary[-1]
-        average_salary = "{0:.5f}".format(total_salary / (len(salary)-2))
-        return float(average_salary)
+        average_salary = round(total_salary / (len(salary)-2) , 5)
+        return average_salary
