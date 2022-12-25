2ce412db570f4e31b81d09a6a7530bd6890b9af7
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-25 20:54:39 +0300

    Time: 117 ms (44.13%), Space: 13.8 MB (72.85%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,5 +1,20 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        num1 = int(num1)
-        num2 = int(num2)
-        return str(num1*num2)
+        num1 = num1[::-1]
+        num2 = num2[::-1]
+        result = [0]*(len(num1)+len(num2))
+        for i,n1 in enumerate(num1):
+            temp = 0
+            index = i
+            for n2 in num2:
+                temp += int(n1)*int(n2) + result[index]
+                result[index] = temp%10 
+                temp //= 10
+                index += 1
+            result[index] += temp
+        ans = ''
+        for x in result:
+            ans = str(x) + ans
+        ans = int(ans)
+        return str(ans)
+        
