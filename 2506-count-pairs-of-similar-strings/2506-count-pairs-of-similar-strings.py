25c914d0e5cce20eb7522a3b36383f614f306a61
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-27 18:39:13 +0300

    Time: 66 ms (93.97%), Space: 13.9 MB (97.56%) - LeetHub

--- 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
+++ 2506-count-pairs-of-similar-strings/2506-count-pairs-of-similar-strings.py
@@ -1,14 +1,13 @@
-import math
+from collections import defaultdict
 class Solution:
     def similarPairs(self, words: List[str]) -> int:
         count = 0
-        dic = {}
+        dic = defaultdict(lambda : 0)
+        
         for i, word in enumerate(words):
-            word = "".join(sorted("".join(set(word))))
-            dic[word] = dic.get(word ,0)+1
-        for i,val in dic.items():
-            for i in range(1,val):
-                count += i
-                
+            word = "".join(sorted(set(word)))
+            count += dic[word]
+            dic[word] += 1
+         
         return count
         
