e8e1269e163500a78849a53ef4153f1e7e712d5a
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-28 00:25:57 +0300

    Time: 136 ms (92.26%), Space: 15.1 MB (79.87%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,10 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         
-        n = max(nums)
-        arr_sum = (n * (n+1) // 2) - sum(nums)
+        n = len(nums)
+        arr_dif = (n * (n+1) // 2) - sum(nums)
         
-        if not arr_sum and 0 in nums:
+        if not arr_dif and 0 in nums:
             return n+1
         else:
-            return arr_sum
+            return arr_dif
