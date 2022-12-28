b17a634aa6becf668c6e3d76b3241b85f0c6ac90
mie21 <103285322+mie21@users.noreply.github.com>
2022-12-29 02:04:46 +0300

    Time: 34 ms (85.75%), Space: 14 MB (9.34%) - LeetHub

--- 0722-remove-comments/0722-remove-comments.py
+++ 0722-remove-comments/0722-remove-comments.py
@@ -17,7 +17,6 @@ class Solution:
                 
                 if last != -1:
                     source[i] = source[i][:idx] + source[i][last+2:]
-    
                     continue
                 
                 source[i] = source[i][:idx]
@@ -25,8 +24,8 @@ class Solution:
                 i += 1   
 
                 while size>i and source[i].find('*/') == -1:
-                    source[i] = ""
-                    i += 1
+                    del source[i] 
+                    size -= 1
                     
                 last = source[i].find('*/')
                 source[temp] += source[i][last+2:]
@@ -40,4 +39,5 @@ class Solution:
                     i += 1
 
         return source
+        
         
