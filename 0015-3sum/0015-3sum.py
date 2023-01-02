32748c3adf916f22a830ac1e9dbeac141926d47b
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-03 00:08:07 +0300

    Time: 5767 ms (11.56%), Space: 17.8 MB (86.68%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,17 +1,22 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        dic = {}
+        nums.sort()
         result = set()
-        # nums.sort()
+        size = len(nums)
         for i,num in enumerate(nums):
-            dic[num] = i
-        for i,num1 in enumerate(nums):
-            for j in range(i+1,len(nums)):
-                temp = -(num1+nums[j])
-                if temp in dic and dic[temp]!=j and dic[temp]!=i:
-                    temp = tuple(sorted((num1,nums[j],temp)))
-                    result.add(temp)
-
-        return map(list,result)
-        
+            num = -num
+            left = i+1
+            right = size-1
+            while right>left:
+                temp = nums[right]+nums[left]
+                if temp==num:
+                    result.add((-num,nums[left],nums[right]))
+                    right -= 1
+                    left += 1
+                elif temp>num:
+                    right -= 1
+                else:
+                    left += 1
+        result = map(list,result)
+        return result
             
