9f573a19953492893ff4d272c056757c5d1038c2
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-07 17:48:55 +0300

    Time: 716 ms (48.92%), Space: 14.4 MB (88.54%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.py
@@ -1,33 +1,24 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
         count = 0
-        beg = end = 0
-        temp = k
-        while len(answerKey)>end:
-            if answerKey[end]=='T':
-                end += 1
-            elif k>0 and answerKey[end]=='F':
-                end += 1
-                k -= 1
-            else:
-                if answerKey[beg]=='F':
-                    k += 1
-                beg += 1
-            count = max(count , end-beg)
+        a,b = 'T','F'
+        for _ in range(2):
+            beg = end = 0 
+            rev = k
+            while len(answerKey)>end:
+                if answerKey[end]==a:
+                    end += 1
+                elif rev>0 and answerKey[end]==b:
+                    end += 1
+                    rev -= 1
+                else:
+                    if answerKey[beg]==b:
+                        rev += 1
+                    beg += 1
+                count = max(count , end-beg)
 
-        beg = end = 0
-        k = temp
-        while len(answerKey)>end:
-            if answerKey[end]=='F':
-                end += 1
-            elif k>0 and answerKey[end]=='T':
-                end += 1
-                k -= 1
-            else:
-                if answerKey[beg]=='T':
-                    k += 1
-                beg += 1
-            count = max(count , end-beg)
-            
+            a = 'F'
+            b = 'T'
+           
         return count
                 
