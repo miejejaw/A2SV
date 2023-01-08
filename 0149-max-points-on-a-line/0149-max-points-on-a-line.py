2cc7e766d04e1d133e92dd7757a72ccde0f77b0d
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-09 00:48:06 +0300

    Time: 110 ms (80.01%), Space: 13.9 MB (75.25%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,17 +1,16 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         
-        
         max_points = 0
         for i in range(len(points)):
-            dic = collections.defaultdict(set)
+            dic = collections.defaultdict(int)
             x1,y1 = points[i]
             for j in range(len(points)):
                 if i==j: continue
                 x2,y2 = points[j]
                 temp = 'v' if x2==x1 else (y2-y1)/(x2-x1)
                 
-                dic[temp].add((x2,y2))
-                max_points = max(max_points,len(dic[temp]))
+                dic[temp] += 1
+                max_points = max(max_points,dic[temp])
                 
         return max_points+1
