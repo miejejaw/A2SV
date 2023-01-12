8273caf00ceffe31e508231d0dd3708a40e17ca2
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-12 11:33:11 +0300

    Time: 52 ms (85.07%), Space: 13.8 MB (99.93%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,20 +1,20 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        dic = [0]*26
+        maindic = [0]*26
         size = len(words)
         for ch in words[0]:
-            dic[ord(ch)-97] += 1
+            maindic[ord(ch)-97] += 1
             
         for ind in range(1,size):
             tempdic = [0]*26
             for ch in words[ind]:
                 tempdic[ord(ch)-97] += 1
-            newdic = [0]*26
-            for ind,freq in enumerate(tempdic):           
-                newdic[ind] = min(tempdic[ind],dic[ind])
-            dic = newdic
+                
+            for i in range(26):           
+                maindic[i] = min(tempdic[i],maindic[i])
+
         ans = []
-        for ind,freq in enumerate(dic): 
+        for ind,freq in enumerate(maindic): 
             ans += [chr(ind+97)]*freq
             
         return ans
