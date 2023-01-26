25f0485c9894906d362379d9266915f4e2b165e0
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-26 18:29:42 +0300

    Time: 454 ms (92.83%), Space: 20.8 MB (66.73%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.py
+++ 0881-boats-to-save-people/0881-boats-to-save-people.py
@@ -4,12 +4,11 @@ class Solution:
         count = 0
         beg,end = 0,len(people)-1
         while end>=beg:
-            if people[end]==limit or people[beg]+people[end]>limit:
-                count += 1
-                end -= 1
-            else:
-                count += 1
-                end -= 1
-                beg += 1
+            if people[beg]+people[end]<=limit:
+                beg += 1  
+                
+            end -= 1
+            count += 1
+                
         return count
             
