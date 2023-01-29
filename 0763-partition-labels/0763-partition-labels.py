ec9f5a1017fecb5c36d723b78fe85179215800de
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-01-29 15:40:09 +0300

    Time: 44 ms (66.18%), Space: 14 MB (14.82%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,25 +1,21 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        dic = defaultdict(list)
-        size = len(s)
+        dic = {}
         for ind,ch in enumerate(s):
             if s[ind] in dic:
                 dic[ch][1] = ind
             else:
-                dic[ch].append(ind)
-                dic[ch].append(ind)
+                dic[ch] = [ind,ind]
         
-        arr = list(dic.values())
         res = []
-        num = arr[0][1]
-        pr = -1
-        for nums in arr:
-            if num>=nums[0]:
-                num = max(num,nums[1])
+        partion = [-1,dic[s[0]][1]]
+        for nums in dic.values():
+            if nums[0]<=partion[1]:
+                partion[1] = max(partion[1],nums[1])
             else:
-                res.append(num-pr)
-                pr = num
-                num = nums[1]
-        res.append(num-pr)
+                res.append(partion[1]-partion[0])
+                partion[0] = partion[1]
+                partion[1] = nums[1]
+        res.append(partion[1]-partion[0])
         
         return res
