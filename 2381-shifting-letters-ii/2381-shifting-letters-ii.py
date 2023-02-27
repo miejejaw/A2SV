2737a4abcf4701ba959f53a774cb0e617bec606d
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-02-27 13:19:05 +0300

    Time: 1337 ms (87.76%), Space: 38.9 MB (89.46%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,14 +1,14 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        
         size = len(s)
         res = [0]*(size+1)
         for shift in shifts:
+            temp = 1
             if shift[2] == 0:
-                res[shift[0]] -= 1
-                res[shift[1]+1] += 1
-            else:
-                res[shift[0]] += 1
-                res[shift[1]+1] -= 1
+                temp = -1
+            res[shift[0]] += temp
+            res[shift[1]+1] += -temp
                 
         res.pop()       
         for ind,val in enumerate(accumulate(res)):
