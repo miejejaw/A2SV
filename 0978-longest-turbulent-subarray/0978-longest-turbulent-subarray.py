725e399489cb2adb5f0c059cd752eb39881747e5
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-02-28 13:34:53 +0300

    Time: 556 ms (33.18%), Space: 17.8 MB (71.18%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -5,11 +5,15 @@ class Solution:
         beg1 = beg2= 0
         
         for end in range(1,size):
-            if (end%2==1 and arr[end-1]<=arr[end]) or (end%2==0 and arr[end-1]>=arr[end]):
-                beg1 = end   
+            if (end%2==1 and arr[end-1]<=arr[end]): 
+                beg1 = end  
+            elif (end%2==0 and arr[end-1]>=arr[end]):
+                beg1 = end 
             res = max(res,end-beg1+1)
       
-            if (end%2==0 and arr[end-1]<=arr[end]) or (end%2==1 and arr[end-1]>=arr[end]):
+            if (end%2==0 and arr[end-1]<=arr[end]):
+                beg2 = end
+            elif (end%2==1 and arr[end-1]>=arr[end]):
                 beg2 = end
             res = max(res,end-beg2+1)
             
