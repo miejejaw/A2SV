855faba81c2bc01f3a188fbb6e22f44baa8fe61b
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-02-28 13:30:06 +0300

    Time: 570 ms (29.44%), Space: 17.8 MB (83.33%) - LeetHub

--- 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -1,10 +1,9 @@
 class Solution:
     def maxTurbulenceSize(self, arr: List[int]) -> int:
         
-        size = len(arr)
-        res = 1
-        beg2 = 0
-        beg1 = 0
+        size,res = len(arr),1
+        beg1 = beg2= 0
+        
         for end in range(1,size):
             if (end%2==1 and arr[end-1]<=arr[end]) or (end%2==0 and arr[end-1]>=arr[end]):
                 beg1 = end   
