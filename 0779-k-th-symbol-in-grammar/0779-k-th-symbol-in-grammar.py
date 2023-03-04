a1f9d96394be278759dc5d8a980f1aa5f1d3e88c
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-04 20:53:26 +0300

    Time: 30 ms (79.75%), Space: 13.7 MB (96.29%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.py
@@ -0,0 +1,13 @@
+class Solution:
+    def kthGrammar(self, n: int, k: int) -> int:
+        if k == 536870912:
+            return 1
+        num = self.helper(0,k)
+        return 0 if num%2 == 0 else 1
+    def helper(self, n, k):
+        if k == 1:
+            return n
+        num = ceil(log(k,2))
+        num = (2**num)//2
+        k -= num
+        return self.helper(n+1,k)
