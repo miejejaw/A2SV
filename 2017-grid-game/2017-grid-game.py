0a716df652b9aff23f8a0df57b832968523ea69b
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-06 14:22:28 +0300

    Time: 1150 ms (80.69%), Space: 31.1 MB (12.74%) - LeetHub

--- 2017-grid-game/2017-grid-game.py
+++ 2017-grid-game/2017-grid-game.py
@@ -4,10 +4,11 @@ class Solution:
             return 0
         grid[0] = list(reversed(list(accumulate(grid[0][::-1]))))
         grid[1] = list(accumulate(grid[1]))
-        ans = [grid[0][1],grid[1][-2]]
+        ans = min(grid[0][1],grid[1][-2])
+        
         for ind in range(1,len(grid[0])-1):
-            ans.append(max(grid[0][ind+1],grid[1][ind-1]))
+            ans = min(ans,max(grid[0][ind+1],grid[1][ind-1]))
             
-        return min(ans)
+        return ans
             
     
