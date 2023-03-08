c130c62c0ecede609ee534de224d32ad9e00e413
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-08 14:46:42 +0300

    Time: 72 ms (67.18%), Space: 26.2 MB (55.27%) - LeetHub

---
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -0,0 +1,18 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        return self.helper(root,p,q)
+    def helper(self,curr,p,q):
+        if curr:
+            if p == curr or q == curr:
+                return curr
+            
+            left = self.helper(curr.left,p,q)
+            right = self.helper(curr.right,p,q)
+            if left and not right:
+                return left
+            elif not left and right:
+                return right
+            elif left and right:
+                return curr
+            
+        return None    
