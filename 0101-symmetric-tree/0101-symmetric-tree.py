d04fae6a3e9114c1eea66d563e39b56c8f793d47
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-08 10:24:38 +0300

    Time: 34 ms (75.45%), Space: 13.9 MB (52.53%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        self.ans = True
+        self.helper(root.left,root.right)
+        return self.ans
+    
+    def helper(self,l,r):
+        if self.ans and l and r:
+            if l.val != r.val:
+                self.ans = False
+            self.helper(l.left, r.right)
+            self.helper(l.right, r.left)
+            return   
+        
+        if l != r:
+            self.ans = False
