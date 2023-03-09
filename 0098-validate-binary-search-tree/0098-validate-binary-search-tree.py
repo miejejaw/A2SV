fb77753190b111657ab1c984f0710cf075ca7c37
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-09 17:40:21 +0300

    Time: 37 ms (96.77%), Space: 16.4 MB (92.21%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,16 +1,17 @@
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        self.ans = []
-        self.isValid(root,None,None)
-        for ind in range(1,len(self.ans)):
-            if self.ans[ind]<=self.ans[ind-1]:
-                return False
-        return True
+        self.ans = True
+        self.num = float("inf")
+        self.isValid(root,False)
+        return self.ans
     
-    def isValid(self,curr,parent,grand):
-        if curr :
+    def isValid(self,curr,parent):
+        if curr and self.ans:
             
-            self.isValid(curr.left,curr,parent)
-            self.ans.append(curr.val)
-            self.isValid(curr.right,curr,grand)
+            self.isValid(curr.right,False)
+            if self.num <= curr.val:
+                self.ans = False
+                return
+            self.num = curr.val
+            self.isValid(curr.left,True)
             
