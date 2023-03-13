68974047e68def9f1de0bc3e6e1d481502e505a4
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-13 12:39:19 +0300

    Time: 966 ms (41.02%), Space: 13.9 MB (23.26%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,19 +1,17 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
-        self.total = sum(cookies)
-        self.ans = self.total
+        self.ans = inf
         self.length = len(cookies)
         cookies.sort(reverse = True)
         self.helper([0]*k,cookies,0,0,k)
         return self.ans
     
     def helper(self, arr, cookies, ind,c,k):
-        if self.total > self.ans < arr[c]:
-            return
         if ind < self.length:
             for j in range(k):
-                arr[j] += cookies[ind]
-                self.helper(arr,cookies,ind+1,j,k)
-                arr[j] -= cookies[ind]  
+                if arr[j] + cookies[ind] < self.ans:
+                    arr[j] += cookies[ind]
+                    self.helper(arr,cookies,ind+1,j,k)
+                    arr[j] -= cookies[ind]  
             return 
         self.ans = min(self.ans,max(arr))
