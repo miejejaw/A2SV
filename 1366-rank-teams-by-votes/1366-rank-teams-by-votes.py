08f32b7443761ba185547eb6fac7b11c773ed884
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-19 16:45:12 +0300

    Time: 79 ms (65.33%), Space: 14.1 MB (86.82%) - LeetHub

---
+++ 1366-rank-teams-by-votes/1366-rank-teams-by-votes.py
@@ -0,0 +1,23 @@
+class Solution:
+    def rankTeams(self, votes: List[str]) -> str:
+        _len = len(votes[0])
+        res = [[]]*26
+        for ch in votes[0]:
+            ind = ord(ch)-65
+            res[ind] = [0]*_len + [-ord(ch)]
+        
+        for vote in votes:
+            for ind in range(_len):
+                num = ord(vote[ind])-65
+                res[num][ind] += 1
+        
+        res.sort(reverse = True)
+        ans = []
+        for v in res:
+            if v:
+                ch = chr(-v[-1])
+                ans.append(ch)
+            
+        return "".join(ans)
+        
+                
