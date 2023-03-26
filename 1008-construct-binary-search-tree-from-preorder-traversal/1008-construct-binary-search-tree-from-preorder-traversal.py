af713a0c64f16cbdae866aeb714cef86286e3ad9
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-03-26 12:21:50 +0300

    Time: 37 ms (69.90%), Space: 14 MB (41.44%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -1,12 +1,9 @@
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
-        self.length = len(preorder)
-        self.ans = TreeNode(preorder[0])
-        self.helper(preorder,1,[self.ans])
-        return self.ans
-    
-    def helper(self,preorder,ind,st):
-        if ind < self.length:
+        length = len(preorder)
+        ans = TreeNode(preorder[0])
+        st = [ans]
+        for ind in range(1,length):
             temp = None
             while st and st[-1].val < preorder[ind]:
                 temp = st.pop()
@@ -18,4 +15,5 @@ class Solution:
                 st[-1].left = newNode
                 
             st.append(newNode)
-            self.helper(preorder,ind+1,st)
+            
+        return ans
