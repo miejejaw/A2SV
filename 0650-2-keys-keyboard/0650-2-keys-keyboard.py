a1148a863f65f5acffcecb2a4503fe55a03b1580
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-04-03 11:23:20 +0300

    Time: 34 ms (77.20%), Space: 13.7 MB (95.61%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,24 +1,19 @@
 class Solution:
     def minSteps(self, n: int) -> int:
         
-        res = []
+        res = 0
         p = 2
         
         while p*p <= n:
             while n%p == 0:
-                res.append(n)
+                res += p
                 n //= p
             p += 1
             
         if n > 1:
-            res.append(n)
-        res.append(1)
-        length = len(res)
-        ans = 0
-        for ind in range(1,length):
-            ans += res[ind-1]//res[ind]
-            
-        return ans
+            res += n
+        
+        return res
             
 
             
