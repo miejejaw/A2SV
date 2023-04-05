d244753d9bd169fc7f31cdf61910351fb7033d6e
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-04-05 15:59:34 +0300

    Time: 40 ms (28.74%), Space: 14.1 MB (26.29%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        self.ans = []
+        self.res = set()
         self.length = len(nums)
-        self.helper([],nums,0)
-        return self.ans
+        self.helper(0,nums,0)
+        return self.h(self.res,nums)
+         
     
-    def helper(self,arr,nums,ind):
+    def helper(self,num,nums,ind):
         if ind < self.length:
             for i in range(ind,self.length):
-                self.helper(arr+[nums[i]],nums,i+1)
-        self.ans.append(arr)
+                self.helper(num | (1<<(i)),nums,i+1)
+        self.res.add(num)
+        
+    def h(self,s,nums):
+        res = []
+        for num in s:
+            temp = []
+            ind = 0
+            for bit in reversed(bin(num)[2:]):
+                if bit == "1":
+                    temp.append(nums[ind])
+                ind += 1
+            res.append(temp)
+        return res
+        
+    
