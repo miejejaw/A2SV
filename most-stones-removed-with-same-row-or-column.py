ec74af7a56f4d03cbc2af638f94457b64f4204f8
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-05-19 18:22:27 +0300

    added most-stones-removed-with-same-row-or-column.python

---
+++ most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,41 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        length = len(stones)
+        rows = defaultdict(list)
+        cols = defaultdict(list)
+        i = 0
+        for x,y in stones:
+            rows[x].append(i)
+            cols[y].append(i)
+            i += 1
+        
+        unionFind = UnionFind(length)
+        for _,row in rows.items():
+            unionFind.union(row)
+        for _,col in cols.items():
+            unionFind.union(col)
+        
+        return unionFind.get()
+        
+class UnionFind:
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+    
+    def get(self):
+        count = 0
+        for i,val in enumerate(self.root):
+            if i != val:
+                count += 1
+        return count
+    def find(self, a):
+        if self.root[a] != a:
+            self.root[a] = self.find(self.root[a]) 
+        return self.root[a]
+
+    def union(self, rows):
+        a = 10000
+        for b in rows:
+            a = min(a,self.find(b))
+        for b in rows:
+            b = self.find(b)
+            self.root[b] = a
