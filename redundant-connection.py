24c8e74d75f2bbee4f21846ecf4be07706ab145e
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-05-19 11:25:10 +0300

    added redundant-connection.python

--- redundant-connection.py
+++ redundant-connection.py
@@ -11,7 +11,6 @@ class Solution:
 class UnionFind:
     def __init__(self, n):
         self.root = [i for i in range(n+1)]
-        self.size = [1]*(n+1)
         
     def find(self, a):
         if self.root[a] != a:
@@ -21,10 +20,6 @@ class UnionFind:
     def union(self, a, b):
         a = self.find(a)
         b = self.find(b)
-        if self.size[a] < self.size[b]:
-            a,b = b,a
-        
-        self.size[a] += self.size[a]
         self.root[b] = a
 
         return a == b
