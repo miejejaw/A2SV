8f05986d2c32638746c2fafe7ea4559b2e364cda
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-05-22 13:19:47 +0300

    added snakes-and-ladders.python

--- snakes-and-ladders.py
+++ snakes-and-ladders.py
@@ -1,13 +1,13 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
 
-        n,ans = len(board),401
+        n = len(board)
         visited = set({1})
         queue = deque([(1,0)])
  
         while queue:
             curr,steps = queue.popleft()
-            if curr == n*n: ans = min(ans,steps)
+            if curr == n*n: return steps
             steps += 1
             for num in range(1,7):
                 num += curr
@@ -19,7 +19,7 @@ class Solution:
                         queue.append((num,steps))
                         visited.add(num)
 
-        return ans if ans != 401 else -1
+        return -1
 
     def getXY(self, num, n):
         row = ceil(num/n)
