32742c63eb030107386036afc0e291d7f1dee788
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-05-30 18:27:08 +0300

    added coin-change.python

--- coin-change.py
+++ coin-change.py
@@ -1,19 +1,15 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0: return 0
-        coins.sort(reverse=True)
-        self.d = defaultdict()
-        for coin in coins:
-            self.d[coin] = 1
+        self.d = {}
         ans = self.dfs(coins,amount)
         return ans if ans<float("inf") else -1
     
     def dfs(self, coins,amount):
         if amount in self.d: return self.d[amount]
-        if amount <= 0: return float("inf")
+        if amount == 0: return 0
         res = float("inf")
         for coin in coins:
-            res = min(res,self.dfs(coins,amount-coin))
-
+            if amount-coin >= 0:
+                res = min(res,self.dfs(coins,amount-coin))
         self.d[amount] = res+1
         return self.d[amount]
