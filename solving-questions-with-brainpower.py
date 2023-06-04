752c37b874a597fb90e2d73fe298939f01fdf591
miejejaw <103285322+miejejaw@users.noreply.github.com>
2023-06-04 10:45:58 +0300

    added solving-questions-with-brainpower.python

--- solving-questions-with-brainpower.py
+++ solving-questions-with-brainpower.py
@@ -2,19 +2,16 @@ class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
 
         self.length = len(questions)
-        self.dp = {}
-        ans = 0
+        self.dp,ans = {},0
         for ind in range(self.length):
             ans = max(ans,self.helper(questions,ind))
         return ans
 
-    def helper(self, questions, ind):
+    def helper(self, nums, ind):
         if ind < self.length:
             if ind in self.dp: return self.dp[ind]
-            res = 0
-            temp = ind+questions[ind][1]+1
-            for i in range(temp,self.length):
-                res = max(res,self.helper(questions,i))
-            self.dp[ind] = res+questions[ind][0]
+            res = self.helper(nums,ind+nums[ind][1]+1)+nums[ind][0]
+            res = max(res,self.helper(nums,ind+1))
+            self.dp[ind] = res
             return self.dp[ind]
         return 0
